a669e841bc3d4ec0cf14b2e6ca4baa9a0c2639b9
manoranjith shankar <ma.ran4073@gmail.com>
2024-05-24 20:52:27 +0530

    [update] links

--- app/redirects/icpdf/page.tsx
+++ app/redirects/icpdf/page.tsx
@@ -5,7 +5,9 @@ import React, { useEffect } from 'react';
 const Files = () => {
 
     useEffect(() => {
+      setTimeout(() => {
         window.location.href = "https://drive.google.com/file/d/1XRIRabxrgsxFTdpyOzG4g0aI6CgvsqtM/view?usp=sharing";
+      }, 2000)
       }, []);
   return (
     <div className="h-screen text-center">
