36da14160bd0c1c53ec496dbd6473fe1c22bb76c
manoranjith shankar <ma.ran4073@gmail.com>
2024-05-29 15:48:03 +0530

    [update] about

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className="relative grid grid-flow-row gap-4 mt-3 mb-3">
         <h1 className={clsx("max-w-10 text-center text-2xl font-bold md:text-3xl lg:text-4xl text-neutral-700 dark:text-foreground underline underline-offset-3 hover:no-underline", fontCaniculeDisplay.className)}> <span className="font-serif">#</span>About</h1>
         <p>
-          Hello there! I&apos;m Manoranjith Shankar, based out of Hosur, India. I&apos;m a Designer and a full Stack Developer with a passion for building secure applications. 
+          Hi! This is Manoranjith Shankar, based out of Hosur, India.
             I code, break things, fix them 🔁. Oh, Just in case if you need my <a href="/redirects/resume" target='_blank' className="no-underline hover:underline underline-offset-4">📄resume</a>.
         </p>
     </div>
